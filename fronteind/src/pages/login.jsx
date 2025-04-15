db5679e3cc29f29f220305b166bb1153e735b7d1
Aman kumar <136961440+Akayaag@users.noreply.github.com>
2025-04-15 22:43:13 +0530

    Update login.jsx

--- fronteind/src/pages/login.jsx
+++ fronteind/src/pages/login.jsx
@@ -28,7 +28,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url =https://firstapp-ivory-theta.vercel.app/auth/login`;
+            const url = firstapp-ivory-theta.vercel.app/auth/login;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
